9acd943b70d4de117862d841e63239bd4bd84584
doctoroyy <doctor.oyy@gmail.com>
2018-09-10 15:39:45 +0800

    modify

--- bin/tool.py
+++ bin/tool.py
@@ -1,8 +1,9 @@
-import urllib.request
+from urllib import request
 
 
 class handle:
+  @staticmethod
   def get_html_doc(url):
-    response = urllib.request.urlopen(url)
+    response = request.urlopen(url)
     html_doc = response.read().decode('utf-8')
     return html_doc
--- core/main.py
+++ core/main.py
@@ -5,7 +5,7 @@ from bin.tool import handle
 if __name__ == "__main__":
   url = "https://www.imdb.com/chart/top"
   imdb_doc = handle.get_html_doc(url)
-  pat = r'<td class="titleColumn">\s*(.*)..*\s*.*\s*title=".*" >(.*)</a>.*\s*<span class="secondaryInfo">(.*)</span>'
+  pat = r'<td class="titleColumn">\s*(.*)..*\s*.*\s*title=".*" >(.*)</a>.*\s*<span class="secondaryInfo">\((.*)\)</span>'
   html_doc = re.findall(pat, imdb_doc)
   wb = Workbook()
   sheet = wb.active
@@ -13,4 +13,4 @@ if __name__ == "__main__":
   for i in range(len(html_doc)):
     for j in range(len(html_doc[i])):
       sheet.cell(row=i + 1, column=j + 1).value = html_doc[i][j]
-  wb.save('E:\IMDB TOP 250\imdb.xlsx')
+  wb.save('imdb.xlsx')
